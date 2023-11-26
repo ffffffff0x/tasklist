7b0b4a3c3d4998573375d32bc58ca7ceb7b6fa74
r0fus0d <sfjm7026516@gmail.com>
2023-11-26 19:51:29 +0800

    Add files via upload

---
+++ JS/linux.js
@@ -0,0 +1,17 @@
+LinuxAvList = {
+    "/sangfor/edr/agent":"深信服edr",
+    "/assist-daemon/assist_daemon":"阿里云云助手守护进程",
+    "zabbix_agentd":"zabbix agen端",
+    "/usr/local/aegis/aegis_update/AliYunDunUpdate":"阿里云盾升级",
+    "/usr/local/aegis/aegis_client/":"阿里云盾",
+    "/usr/local/share/aliyun-assist":"阿里云助手",
+    "AliHips":"阿里系监控",
+    "AliNet":"阿里系监控",
+    "AliDetect":"阿里系监控",
+    "AliScriptEngine":"阿里系监控",
+    "secu-tcs-agent":"腾讯系监控",
+    "/usr/local/qcloud/stargate/":"腾讯系监控",
+    "/usr/local/qcloud/monitor/":"腾讯系监控",
+    "/usr/local/qcloud/YunJing/":"腾讯系监控",
+    "/usr/local/qcloud/tat_agent/":"腾讯自动化助手TAT产品",
+};
